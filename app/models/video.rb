461a368e3743ea88df1661a85e9062b099f655eb
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-05 11:36:51 -0700

    models

---
+++ app/models/video.rb
@@ -0,0 +1,3 @@
+class Video < ApplicationRecord
+  has_many :comments, dependent: :destroy 
+end
---
+++ db/migrate/20190105182028_create_videos.rb
@@ -0,0 +1,14 @@
+class CreateVideos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.string :category
+      t.string :duration
+      t.string :genre
+      t.string :description
+      t.string :trailer
+
+      t.timestamps
+    end
+  end
+end

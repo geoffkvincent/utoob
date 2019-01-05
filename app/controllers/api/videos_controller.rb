a2699c3d96061cd6aba474a418683e2dc218a84d
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-05 11:56:57 -0700

    video controller

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,16 +1,43 @@
 class Api::VideosController < ApplicationController
+  before_action :set_video, only: [:show, update, :destroy]
+
   def index
+    render json: current_user.videos
   end
 
   def show
+    render json: @video
   end
 
   def create
+    video = current_user.video.new(video_params)
+
+    if video.save
+      render json: video
+    else
+      render json: video.errors, status 422
+    end
   end
 
   def update
+    if @video.update(video_params)
+      redner json: @video
+    else
+      render json: @video.errors, status 422
+    end
   end
 
   def destroy
+    @video.destroy
+  end
+
+  private
+
+  def set_video
+    @video = currents_user.videos.find(params[:id])
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :category, :duration, :genre, :description, :trailer)
   end
 end

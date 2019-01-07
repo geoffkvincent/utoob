adf2227c3643598531ebbb928f48deb88f685c8a
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-06 20:30:45 -0700

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
+import NoMatch from './components/NoMatch'
 
 const App = () => (
   <Fragment>

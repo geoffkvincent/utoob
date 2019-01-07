86097b4d6382028b53959569e10874e15a4dee5b
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-06 20:29:18 -0700

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from 'react'
 import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
 
 const App = () => (
   <Fragment>
     <Switch>
-      <Route />
+      <Route exact path="/" component={Home} />
     </Switch>
   </Fragment>
 )

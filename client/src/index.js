d591200216a4cc93c50d184c4562f2393014a932
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-06 20:34:04 -0700

    app

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import {AuthProvider} from "./providers/AuthProvider"
 import 'semantic-ui-css/semantic.min.css'
+import {initMiddleware} from 'devise-axios'
 import App from './App';
+
+initMiddleware()
 
 ReactDOM.render(
   <AuthProvider>
